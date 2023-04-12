fd9ff5b1d9d42afb50d319b2dfd626f50a6a2003
harsh jaiswal <tech.harsh2003@gmail.com>
2023-04-12 08:45:45 +0530

    second comit

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -4,10 +4,10 @@ import { Firebase } from "../../firebase/config";
 import { useHistory } from "react-router";
 import "./View.css";
 function View() {
-  let { postContent } = useContext(PostContext);//from the global store PostContext we can get information about desired product post that we want to show (the user is clicked item on the card)
+  let { postContent } = useContext(PostContext); //from the global store PostContext we can get information about desired product post that we want to show (the user is clicked item on the card)
 
-  const [userDetails, setUserDetails] = useState();//we want show the details of who is posted the add and we dont know,so we want retreive user data from firebase who is posted this add
-  const history = useHistory();//if user click the refresh of the page then PostContext data will be erased so it will throws an error so that time we want redirect this page to home page
+  const [userDetails, setUserDetails] = useState(); //we want show the details of who is posted the add and we dont know,so we want retreive user data from firebase who is posted this add
+  const history = useHistory(); //if user click the refresh of the page then PostContext data will be erased so it will throws an error so that time we want redirect this page to home page
   useEffect(() => {
     let { userId } = postContent;
     if (userId === undefined) {
@@ -31,24 +31,29 @@ function View() {
       </div>{" "}
       <div className="rightSection">
         <div className="productDetails">
-          <p>&#x20B9; {postContent.price} </p>
-          <span>{postContent.name}</span>
-          <p>{postContent.category}</p>
+          <p>
+            <span style={{ fontSize: "20px" }}>Requested Amount:</span> &#x20B9;{" "}
+            {postContent.fund} Lakh
+          </p>
+          <span>Company Name: {postContent.cname}</span>
+          <p>Founder Name: {postContent.name}</p>
+          <p>LinkedIn: {postContent.linkedin}</p>
+          <span>Gross Margin: {postContent.gMargin}</span>
+          <p>Net Margin: {postContent.nMargin}</p>
+          <p>Equity: {postContent.equity}%</p>
           <span>{postContent.createdAt}</span>
         </div>
         <div className="productDescription">
-            <p className="p-bold">Product Description</p>
-            <p>{postContent.description}</p>
-            
-          </div>
-        {userDetails &&
+          <p className="p-bold">Product Description</p>
+          <p>{postContent.description}</p>
+        </div>
+        {userDetails && (
           <div className="contactDetails">
-            <p className="p-bold">Seller details</p>
+            <p className="p-bold">Founder details</p>
             <p>Name : {userDetails.name}</p>
             <p>Phone : {userDetails.phone}</p>
           </div>
-        }
-       
+        )}
       </div>
     </div>
   );
